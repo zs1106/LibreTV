71245f5e09fab8b891caebddb8bbe7343a21413a
zs1106 <105514409+zs1106@users.noreply.github.com>
2025-07-13 03:02:21 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -126,43 +126,35 @@ const API_SITES = {
     // ckzy: {
     //     api: 'https://www.ckzy1.com',
     //     name: 'CK资源',
-    //     adult: true
     // },
     // jkun: {
     //     api: 'https://jkunzyapi.com',
     //     name: 'jkun资源',
-    //     adult: true
     // },
     // bwzy: {
     //     api: 'https://api.bwzym3u8.com',
     //     name: '百万资源',
-    //     adult: true
     // },
     // souav: {
     //     api: 'https://api.souavzy.vip',
     //     name: 'souav资源',
-    //     adult: true
     // },
     // r155: {
     //     api: 'https://155api.com',
     //     name: '155资源',
-    //     adult: true
     // },
     // lsb: {
     //     api: 'https://apilsbzy1.com',
     //     name: 'lsb资源',
-    //     adult: true
     // },
     // huangcang: {
     //     api: 'https://hsckzy.vip',
     //     name: '黄色仓库',
-    //     adult: true,
     //     detail: 'https://hsckzy.vip'
     // },
     // yutu: {
     //     api: 'https://yutuzy10.com',
     //     name: '玉兔资源',
-    //     adult: true
     // },
 
     // 下面是资源失效率高的API源，不建议使用
